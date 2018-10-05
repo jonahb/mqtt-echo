d0eff7b314775c1beb779f1aecad850bba982223
Jonah Burke <jonah@jonahb.com>
2018-10-05 13:13:37 -0400

    Reformat EchoServer

--- unity/Assets/EchoServer.cs
+++ unity/Assets/EchoServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 using Encoding = System.Text.Encoding;
 
@@ -11,32 +12,33 @@ using MqttClient = uPLibrary.Networking.M2Mqtt.MqttClient;
 using MqttMsgBase = uPLibrary.Networking.M2Mqtt.Messages.MqttMsgBase;
 using MqttMsgPublishEventArgs = uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs;
 
-public class EchoServer : MonoBehaviour {
-	private MqttClient mqttClient;
+public class EchoServer : MonoBehaviour
+{
+    private MqttClient mqttClient;
 
-	public string topic = "moey";
-	public string echoTopic = "moey_echo";
-	public string mqttBrokerHost = "test.mosquitto.org";
+    public string topic = "moey";
+    public string echoTopic = "moey_echo";
+    public string mqttBrokerHost = "test.mosquitto.org";
 
-	void Start() {
-		mqttClient = new MqttClient(mqttBrokerHost);
-		mqttClient.MqttMsgPublishReceived += HandleClientMqttMsgPublishReceived;
+    private void Start() {
+        mqttClient = new MqttClient(mqttBrokerHost);
+        mqttClient.MqttMsgPublishReceived += HandleClientMqttMsgPublishReceived;
 
-		String clientId = Guid.NewGuid().ToString();
-		mqttClient.Connect(clientId);
+        String clientId = Guid.NewGuid().ToString();
+        mqttClient.Connect(clientId);
 
-		string[] topics = { topic };
-		byte[] qualityOfServiceLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
-		mqttClient.Subscribe(topics, qualityOfServiceLevels);
-	}
+        string[] topics = { topic };
+        byte[] qualityOfServiceLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
+        mqttClient.Subscribe(topics, qualityOfServiceLevels);
+    }
 
-	void HandleClientMqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) {
-		JSONNode obj = JSON.Parse(Encoding.UTF8.GetString(e.Message));
-		string message = obj["message"];
-		Debug.Log(message);
+    private void HandleClientMqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) {
+        JSONNode obj = JSON.Parse(Encoding.UTF8.GetString(e.Message));
+        string message = obj["message"];
+        Debug.Log(message);
 
-		JSONNode echoObj = new JSONObject();
-		echoObj["message"] = message;
-		mqttClient.Publish(echoTopic, Encoding.UTF8.GetBytes(echoObj.ToString()));
-	}
+        JSONNode echoObj = new JSONObject();
+        echoObj["message"] = message;
+        mqttClient.Publish(echoTopic, Encoding.UTF8.GetBytes(echoObj.ToString()));
+    }
 }
